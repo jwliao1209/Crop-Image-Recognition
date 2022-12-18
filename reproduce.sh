a6ccebdf988e7d88bd8d619a33f49a4e15b7f2bf
Yi-Cheng Hung <93210989+YCHung1998@users.noreply.github.com>
2022-12-18 19:42:02 +0800

    Update reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -17,7 +17,3 @@ CUDA_VISIBLE_DEVICES=0 python inference.py --checkpoint 12-03-14-52-49 --topk 3
 wait
 
 CUDA_VISIBLE_DEVICES=0 python inference.py --checkpoint 12-06-10-30-56 --topk 3
-
-wait
-
-python generate_merge_csv.py
