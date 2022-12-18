c1a0ce4c16a16376a776b10e4c32ef536e016e41
Yi-Cheng Hung <93210989+YCHung1998@users.noreply.github.com>
2022-12-18 19:38:40 +0800

    Update generate_merge_csv.py

--- generate_merge_csv.py
+++ generate_merge_csv.py
@@ -10,17 +10,18 @@ def read_csv(path):
     return pd.read_csv(path).sort_values(by=['filename'], ignore_index=True)
 
 
-def main():
-    ckpt_list = [
-        '11-23-11-31-04_top3_submission.csv',
-        '11-25-19-31-53_top3_submission.csv',
-        '12-03-14-52-49_top3_submission.csv',
-        '12-06-10-30-56_top3_submission.csv',
-        'regent_v1.csv',
-        'swin_s_1080.csv',
-        'swin_b_512.csv',
-        'swin_b_1080.csv',
-        ]
+def main(ckpt_list=None):
+    if not ckpt_list:
+        ckpt_list = [
+            '11-23-11-31-04_top3_submission.csv',
+            '11-25-19-31-53_top3_submission.csv',
+            '12-03-14-52-49_top3_submission.csv',
+            '12-06-10-30-56_top3_submission.csv',
+            'regent.csv',
+            'swin_b.csv',
+            'swin_b_1080.csv',
+            'swin_s_1080.csv',
+            ]
 
     df_list = [read_csv(os.path.join('submission', ckpt)) for ckpt in ckpt_list]
     label_list = np.array([df['label'].to_list() for df in df_list])
@@ -33,4 +34,5 @@ def main():
     print(f'Merge size: {labels.shape}')
 
 if __name__ == '__main__':
-    main()
+    ckpt_list = os.listdir('submission')    
+    main(ckpt_list)
